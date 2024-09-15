43f20d903041b9cedfebda2bd1b1a2d40f6019be
KyrA-github <korenovsostav@gmail.com>
2024-09-15 18:29:11 +0300

    +file

---
+++ Debug/Maps/2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="2" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="2.png" width="300" height="300"/>
+</tileset>
